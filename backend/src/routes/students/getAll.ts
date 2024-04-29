0c61a89bd2b4e38eb0b1c5ee4b2ca1dcce38fe84
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-04-28 23:22:40 -0300

    feat: add route and controller for get all students

---
+++ backend/src/routes/students/getAll.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance, FastifyReply } from 'fastify';
+import { studentsController } from 'src/controllers/students';
+import { StudentRepository } from 'src/repositories/StudentRepository';
+
+export const getAll = async (app: FastifyInstance) => {
+  app.get('/alunos', async (_, reply: FastifyReply) => {
+    try {
+      const repository = new StudentRepository();
+      const result = await studentsController.getAll(repository);
+      reply.send(result);
+    } catch (error: any) {
+      reply.status(400).send({ error: error.message });
+    }
+  });
+};
--- backend/src/routes/students/students.routes.ts
+++ backend/src/routes/students/students.routes.ts
@@ -1,6 +1,8 @@
 import { app } from 'src/server';
 import { createStudent } from './createStudent';
+import { getAll } from './getAll';
 
 export const studentsRoutes = async () => {
+  app.register(getAll);
   app.register(createStudent);
 };
