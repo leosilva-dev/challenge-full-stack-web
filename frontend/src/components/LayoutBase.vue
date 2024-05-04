828ddc28fc24818af5d759f69b270acebf87b858
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-05-04 11:18:45 -0300

    feat: add LayoutBase component

---
+++ frontend/src/components/LayoutBase.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const toggleDrawerOpen = ref(true)
+const router = useRouter()
+
+const items = [
+  {
+    title: 'Página Inicial',
+    icon: 'mdi-home',
+    route: '/'
+  }
+]
+
+const navigateTo = (route: string) => {
+  router.push(route)
+}
+</script>
+<template>
+  <v-card>
+    <v-layout>
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="toggleDrawerOpen = !toggleDrawerOpen"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>Módulo acadêmico</v-toolbar-title>
+        <v-spacer />
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="toggleDrawerOpen">
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index" @click="navigateTo(item.route)">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 95vh">
+        <RouterView />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
---
+++ frontend/src/components/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import LayoutBase from '../components/LayoutBase.vue'
+</script>
+
+<template>
+  <div>
+    <RouterView />
+    <LayoutBase />
+  </div>
+</template>
