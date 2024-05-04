a54cf59ba4bdacba559a26e0aaa6ba6ff7dc6ecb
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-05-04 11:19:20 -0300

    feat: add MainLayout on routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainLayout from '../components/MainLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: HomeView
+        }
+      ]
     }
   ]
 })
