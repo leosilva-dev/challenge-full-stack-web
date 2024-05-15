c44112dddd91f03e76e7bbd81f297331b3594ba4
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-05-15 17:43:23 -0300

    feat: add students detail page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from '../components/MainLayout.vue'
 import HomeView from '@/views/HomeView.vue'
-import StudentsListView from '@/views/StudentsListView.vue'
+import StudentsListView from '@/views/students/StudentsListView.vue'
+import StudentsDetailsView from '@/views/students/StudentsDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,7 +25,18 @@ const router = createRouter({
         {
           path: '',
           name: 'alunos',
-          component: StudentsListView
+          component: StudentsListView,
+          meta: {
+            title: 'Consulta de alunos'
+          }
+        },
+        {
+          path: ':id',
+          name: 'detalhesAluno',
+          component: StudentsDetailsView,
+          meta: {
+            title: 'Detalhes alunos'
+          }
         }
       ]
     }
---
+++ frontend/src/views/students/StudentsDetailsView.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { onMounted, reactive, ref, watch } from 'vue'
+import { StudentsService, type IStudent } from '@/services/StudentsServices'
+
+const router = useRouter()
+const studentId = ref<string>('')
+const isEditing = ref<boolean>(false)
+const student = reactive<IStudent>({} as IStudent)
+
+const showAlert = ref(false)
+const alertText = ref('')
+const snackbarColor = ref('')
+
+const allFieldsFilled = ref(false)
+
+const validateFields = () => {
+  allFieldsFilled.value = !!student.ra && !!student.name && !!student.email && !!student.cpf
+}
+
+watch([student], () => {
+  validateFields()
+})
+
+onMounted(() => {
+  if (router.currentRoute.value.params.id) {
+    studentId.value = router.currentRoute.value.params.id.toString()
+    if (studentId.value !== 'novo') {
+      isEditing.value = true
+      fetchStudent(parseInt(studentId.value))
+    }
+  }
+})
+
+const fetchStudent = (studentId: number) => {
+  StudentsService.getById(studentId).then((response) => {
+    if (response) {
+      Object.assign(student, response)
+      console.log(response)
+    }
+  })
+}
+
+const handleCancel = () => {
+  router.push(`/alunos`)
+}
+
+const displayAlert = (message: string, success: 'success' | 'error') => {
+  alertText.value = message
+  snackbarColor.value = success === 'success' ? 'green' : 'red'
+  showAlert.value = true
+}
+
+const handleSave = () => {
+  if (!allFieldsFilled.value) {
+    displayAlert('Por favor, preencha todos os campos obrigatórios.', 'error')
+    return
+  }
+
+  if (isEditing.value) {
+    StudentsService.update(student).then((response) => {
+      if (response) {
+        displayAlert('Usuário editado com sucesso.', 'success')
+      } else {
+        displayAlert('Erro ao editar usuário.', 'error')
+      }
+    })
+  } else {
+    StudentsService.create(student).then((response) => {
+      if (response) {
+        displayAlert('Usuário cadastrado com sucesso.', 'success')
+      } else {
+        displayAlert('Erro ao cadastrar usuário.', 'error')
+      }
+    })
+  }
+}
+</script>
+
+<template>
+  <v-snackbar :color="snackbarColor" v-model="showAlert">
+    {{ alertText }}
+  </v-snackbar>
+  <v-container class="elevation-3 mt-8 w-50">
+    <h1>{{ isEditing ? 'Editar Aluno' : 'Cadastrar Aluno' }}</h1>
+
+    <v-form @submit.prevent>
+      <v-col cols="12">
+        <v-text-field v-model="student.ra" label="RA" :disabled="isEditing" required></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field v-model="student.name" label="Nome" required></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field v-model="student.email" label="E-mail" required></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="student.cpf"
+          label="CPF"
+          :disabled="isEditing"
+          required
+        ></v-text-field>
+      </v-col>
+
+      <v-btn color="primary" variant="outlined" class="mr-4" @click="handleCancel">Cancelar</v-btn>
+      <v-btn type="submit" color="primary" @click="handleSave" :disabled="!allFieldsFilled">{{
+        isEditing ? 'Salvar' : 'Cadastrar'
+      }}</v-btn>
+    </v-form>
+  </v-container>
+</template>
