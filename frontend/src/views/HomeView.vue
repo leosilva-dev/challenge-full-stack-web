cacf20f8560f76811ff6e6bc5fdd6f88a24e3e51
Leonardo Silva <silvaleonardo.ti@gmail.com>
2024-05-15 23:10:08 -0300

    feat: add a card to navigate to students page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToStudentsPage = () => {
+  router.push('/alunos')
+}
+</script>
 
 <template>
   <main>
-    <h1>HomeView</h1>
+    <v-container fluid>
+      <v-row justify="start">
+        <v-col cols="12" md="4">
+          <v-card class="shortcut-card" @click="goToStudentsPage">
+            <v-card-title>
+              <span class="headline">Veja todos os alunos</span>
+            </v-card-title>
+            <v-card-text>Explore a lista de alunos e suas informações.</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
