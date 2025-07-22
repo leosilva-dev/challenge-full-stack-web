6235132d1b90c4ef3dd41f17b090602f0cf4e4e8
Leonardo Silva <silvaleonardo.ti@gmail.com>
2025-07-22 04:37:43 -0300

    feat: add authentication and admin middleware to student routes

--- backend/src/routes/students.route.ts
+++ backend/src/routes/students.route.ts
@@ -1,10 +1,30 @@
 import { FastifyInstance } from 'fastify';
 import { studentsController } from '../controllers/students/student.controller';
+import { authMiddleware, adminMiddleware } from '../middlewares/auth.middleware';
 
 export const studentsRoutes = async (app: FastifyInstance) => {
-  app.post('/students', studentsController.createStudent);
-  app.get('/students', studentsController.getAllStudents);
-  app.get('/students/:id', studentsController.getStudentById);
-  app.put('/students/:id', studentsController.updateStudent);
-  app.delete('/students/:id', studentsController.deleteStudent);
+  app.post('/students', {
+    preHandler: [authMiddleware],
+    handler: studentsController.createStudent,
+  });
+
+  app.get('/students', {
+    preHandler: [authMiddleware],
+    handler: studentsController.getAllStudents,
+  });
+
+  app.get('/students/:id', {
+    preHandler: [authMiddleware],
+    handler: studentsController.getStudentById,
+  });
+
+  app.put('/students/:id', {
+    preHandler: [authMiddleware],
+    handler: studentsController.updateStudent,
+  });
+
+  app.delete('/students/:id', {
+    preHandler: [adminMiddleware],
+    handler: studentsController.deleteStudent,
+  });
 };
